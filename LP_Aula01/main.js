2b8bd8e03917ce782113534d061e6cf50c91945c
MileneMicheloti <130112599+MileneMicheloti@users.noreply.github.com>
2024-04-01 22:27:04 -0300

    Add files via upload

---
+++ LP_Aula01/main.js
@@ -0,0 +1,27 @@
+const nome = 'Milene'
+const sobrenome =   'Micheloti'
+const idade = '19'
+
+/* console.log(nome.charAt(3))
+console.log(nome.charCodeAt(3))
+console.log(nome.replace('bi', 'xx'))
+console.log(nome.length)
+console.log(nome.endsWith('n'))
+console.log(nome.indexOf('b'))
+console.log(nome.substring(o, 3))
+console.log(nome.toLocaleUpperCase())
+console.log('Ana,Maria,Jose'.split(','))
+console.log(nome.concat('Milene'.concat('Micheloti')))
+if (nome.includes('i')) {} */
+
+console.log('Milene'+' '+"Micheloti")
+console.log(`Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos`)
+
+function soma(a, b){
+    return a+b
+}
+console.log(soma(2,3))
+
+for(let i=0; i<5; i++){
+    
+}
